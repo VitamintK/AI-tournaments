b619f241f92e7722010a59ff80cce97ed1138c7e
KEvin Wang <kevinwang@kevinwang.us>
2015-02-11 07:13:58 -0800

    we oop now

--- simplestpoker.py
+++ simplestpoker.py
@@ -2,20 +2,29 @@
 import random
 
 class Player:
-    def __init__(self, number = None):
-        self.number = number
+    def __init__(self, money = 1000):
+        self.number = None
+        self.money = money
     def move(self):
         pass
     def get_dealt(self, max_num = 1):
         """Deal the player a new number"""
         self.number = random.random() * max_num
 
-def run_game(num_of_players = 3):
-    players = [Player() for i in range(num_of_players)]
+class Poker_Board:
+    def __init__(self, num_of_players = 3, init_money = 1000):
+        self.players = [Player(money = init_money) for i in range(num_of_players)]
+        self.pot = 0
+    
+
+    
+
+def run_game(num_of_players = 3, starting_money = 1000):
+    pot = 0
     while True:
         for player in players:
             player.get_dealt()
             print(player.number)
 
-run_game()
+run_game(3, 1000)
         
